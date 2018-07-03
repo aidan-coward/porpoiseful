35c2533664231665945c44f506cae4355be08ec0
Aidan Coward <aidan.coward@gmail.com>
2018-07-03 13:45:42 -0400

    should be done changing name

--- src/lib.rs
+++ src/lib.rs
@@ -6,85 +6,108 @@ enum DisplayValue {
 
 /// The current percentage of battery remaining
   BatteryLifePercentage,
-/// 
-  CPULoadAverage,
+
 /// The average load of all the cpu cores in percent
+  CPULoadAverage,
   
-  CPULoadCore0,
 /// the load of the 0th core
+  CPULoadCore0,
 
-  CPULoadCore1,
 /// the load of the 1st core
+  CPULoadCore1,
 
-  CPULoadCore2,
 /// the load of the 2nd core
+  CPULoadCore2,
 
-  CPULoadCore3,
 /// the load of the 3rd core
+  CPULoadCore3,
 
-  CPULoadCore4,
 /// the load of the 4th core
+  CPULoadCore4,
 
-  CPULoadCore5,
 /// the load of the 5th core
+  CPULoadCore5,
 
-  CPULoadCore6,
 /// the load of the 6th core
+  CPULoadCore6,
 
-  CPULoadCore7,
 /// the load of the 7th core
+  CPULoadCore7,
 
-  CPUTemperatureAverage,
 /// Average temperature of all the cpu cores
+  CPUTemperatureAverage,
 
-  CPUTemperatureCore0,
 /// The temperature of the 0th core
+  CPUTemperatureCore0,
 
-  CPUTemperatureCore1,
 /// The temperature of the 1st core
+  CPUTemperatureCore1,
 
-  CPUTemperatureCore2,
 /// The temperature of the 2nd core
+  CPUTemperatureCore2,
 
-  CPUTemperatureCore3,
 /// The temperature of the 3rd core
+  CPUTemperatureCore3,
 
-  CPUTemperatureCore4,
 /// The temperature of the 4th core
+  CPUTemperatureCore4,
 
-  CPUTemperatureCore5,
 /// The temperature of the 5th core
+  CPUTemperatureCore5,
 
-  CPUTemperatureCore6,
 /// The temperature of the 6th core
+  CPUTemperatureCore6,
 
-  CPUTemperatureCore7,
 /// The temperature of the 7th core
+  CPUTemperatureCore7,
 
-  GPULoad1,
 /// The load of the 1st GPU in percentage
+  GPULoad0,
 
-  GPULoad2,
 /// The load of the 2nd GPU in percentage
+  GPULoad1,
 
-  GPUTemperature1,
 /// The temperature of the 0th GPU
+  GPUTemperature1,
 
-  GPUTemperature2,
 /// The temperature of the 1st GPU
+  GPUTemperature2,
 
-  RAMUsage,
 /// Usage of RAM
+  RAMUsage,
 
-  RAMUsagePercentage,
 /// Usage of RAM in percentage
+  RAMUsagePercentage,
 
+/// System date 
   Date,
-  Time,
-  AudioVolume,
+
+/// System time in 12 hour format
+  Time12Hour,
+
+///System time in 24 hour format
+  Time24Hour,
+
+/// Audio volume alsa
+  AudioVolumeAlsa,
+
+/// Audio volume oss
+  AudioVolumeOSS,
+
+/// Audio volume pulseaudio
+  AudioVolumePulseAudio,
+
+/// The usage of the 0th hard drive
+  HardDriveUsage0,
+
+/// The usage of the 0th hard drive in percent
+  HardDriveUsagePercentage0,
+
+/// The usage of the 1st hard drive
   HardDriveUsage1,
+
+/// The usage of the 1st hard drive in percent
   HardDriveUsagePercentage1,
-  HardDriveUsage2,
-  HardDriveUsagePercentage2,
+
 }
 
